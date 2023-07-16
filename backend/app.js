0814325dc18f7ff82a59d6ffe2125d6eef393e10
Igor Malinavichus <crazych4tty@gmail.com>
2023-07-16 19:42:03 +0300

    feat: Переименовать порт обратно на 3000 и выгрузить на github

--- backend/app.js
+++ backend/app.js
@@ -8,7 +8,7 @@ const NotFoundError = require('./errors/not-found-error');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 const cors = require('./middlewares/cors');
 
-const { PORT = 3005 } = process.env;
+const { PORT = 3000 } = process.env;
 const app = express();
 app.use(cors);
 
